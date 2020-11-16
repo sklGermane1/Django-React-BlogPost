d44fc85cf814f161e34a9f3eabdbf559fce4fdec
Bill <billcrafter007@gmail.com>
2020-11-16 17:55:10 +0100

    Implemented react in django

---
+++ mysite/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+const App = () => {
+    return (
+        <div>
+            <h1>React</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,document.getElementById("app"))
+
+export default App
