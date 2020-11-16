47268df61bdc57f926b3912cf45453723d9b9d48
Bill <billcrafter007@gmail.com>
2020-11-16 17:37:11 +0100

    created Post model and serializer

--- mysite/backend/views.py
+++ mysite/backend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets, permissions
 
 # Create your views here.
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.AllowAny
+    ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "knox",
+    "rest_framework",
+    "accounts.apps.AccountsConfig",
+    "frontend.apps.FrontendConfig",
+    "backend.apps.BackendConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
